db908cb0fb1a0864c1d23f7bd8a6f02d983eab5b
vbertazzo <vbertazzo@gmail.com>
2021-08-30 23:38:46 -0300

    Complete exercise 02

---
+++ desafio-01/src/Content.jsx
@@ -0,0 +1,5 @@
+const Content = ({ children }) => {
+  return <article className='content'>{children}</article>
+}
+
+export default Content
--- desafio-01/src/app.js
+++ desafio-01/src/app.js
@@ -1,3 +1,22 @@
-const App = () => <h1>"App"</h1>
+import Header from './Header'
+import Menu from './Menu'
+import Main from './Main'
+import Sidebar from './Sidebar'
+import Content from './Content'
+import Footer from './Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Main>
+        <Sidebar />
+        <Content />
+      </Main>
+      <Footer />
+    </>
+  )
+}
 
 export default App
