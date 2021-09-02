41600d8cc8dbb090eb7c32b97395088958ebe54c
vbertazzo <vbertazzo@gmail.com>
2021-09-02 13:53:34 -0300

    Challenge 01: style refactor

--- desafio-01/src/app.js
+++ desafio-01/src/app.js
@@ -1,6 +1,5 @@
 import Header from './components/layout/header'
 import Menu from './components/layout/menu'
-import Main from './components/layout/main'
 import Sidebar from './components/layout/sidebar'
 import Content from './components/layout/content'
 import Footer from './components/layout/footer'
@@ -11,30 +10,25 @@ const App = () => {
   return (
     <>
       <Header>
-        <H1>Bootcamp B-Academy</H1>
+        <H1>B. Academy Bootcamp</H1>
       </Header>
       <Menu>
-        <a href='/bootcamp'>Bootcamp</a>
-        <a href='/sobre'>Sobre a Brainn</a>
+        <a href=''>Grade do Curso</a>
       </Menu>
-      <Main>
-        <Sidebar>
-          <H2>Tópicos</H2>
-          <ul>
-            <li>JSX e Virtual DOM</li>
-            <li>Create React App</li>
-            <li>DevTools</li>
-            <li>Props</li>
-            <li>Prop "children"</li>
-          </ul>
-        </Sidebar>
-        <Content>
-          <H2>Pronto para aprender mais sobre React?</H2>
-          <p>Se inscreva para participar do bootcamp.</p>
-          <Button kind='primary'>Me inscrever</Button>
-          <Button kind='secondary'>Saber mais</Button>
-        </Content>
-      </Main>
+      <Sidebar>
+        <H2>Conteúdo do Bootcamp</H2>
+        <ul>
+          <li>Introdução ao React.js</li>
+          <li>JSX</li>
+          <li>Styled Components</li>
+        </ul>
+      </Sidebar>
+      <Content>
+        <H2>Pronto para aprender mais sobre React?</H2>
+        <p>Se inscreva para participar do bootcamp.</p>
+        <Button kind='primary'>Me inscrever</Button>
+        <Button kind='secondary'>Saber mais</Button>
+      </Content>
       <Footer>
         <H3>Brainn Co.</H3>
         <a href='mailto:contato-inexistente@brainn.co'>Contato</a>
