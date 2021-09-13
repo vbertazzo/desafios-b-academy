a027d319271888bd4bdccc6304cd468536a3c40e
vbertazzo <vbertazzo@gmail.com>
2021-09-13 18:28:34 -0300

    Challenge 03: refactor remove car function

--- desafio-03/app/src/app.js
+++ desafio-03/app/src/app.js
@@ -63,7 +63,7 @@ function App () {
       setError({ active: false, message: '' })
     }
 
-    setCars(cars.filter(car => car.plate !== plate))
+    setCars(prevState => prevState.filter(car => car.plate !== plate))
   }
 
   return (
